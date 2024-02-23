4808440cb23e3b2060f18bb481992e1afba680f4
eveeify <eveeg1971@gmail.com>
2024-02-24 02:02:42 +1100

    feat: mdx & page init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   redirects: {
     "/github": "https://github.com/Cordevall"
   },
-  integrations: [tailwind()]
+  integrations: [tailwind(), mdx()]
 });
