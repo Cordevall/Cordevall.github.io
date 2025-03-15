99fcba0eb5c45c7018346ca06153a65e4d108fd6
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2025-03-16 03:06:09 +1100

    feat: content collections

---
+++ src/contents.config.ts
@@ -0,0 +1,32 @@
+import { z, defineCollection } from 'astro:content';
+import { glob } from 'astro/loaders';
+
+const blog = defineCollection({
+	loader: glob({ pattern: '**/*.md', base: 'src/content/blog' }),
+	schema: z.object({
+		title: z.string(),
+		date: z.date(),
+		desc: z.date(),
+	}),
+});
+
+const news = defineCollection({
+	loader: glob({ pattern: '**/*.md', base: 'src/content/news' }),
+	schema: z.object({
+		title: z.string(),
+		date: z.date(),
+		desc: z.string(),
+	}),
+})
+
+const author = defineCollection({
+	loader: glob({ pattern: '**/*.json', base: 'src/content/authors' }),
+	schema: z.object({
+		name: z.string(),
+		role: z.string(),
+		img: z.string(),
+	}),
+});
+
+
+export const collections = { blog, news, author };
